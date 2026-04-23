b322451ed5498fb7b009628d0519a06705950fc3
Gigscourt <theprimestarventures@gmail.com>
2026-04-23 10:12:01 +0100

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -8,7 +8,7 @@ const REDIRECT_URI = isCapacitor
   ? 'com.gigscourt.app://oauth2redirect' // For Capacitor Native App
   : 'https://gigscourt-capacitor.vercel.app/oauth-redirect'; // For Web App
 
-const AUTHGEAR_CLIENT_ID = 'b41661345e851ffc';
+const AUTHGEAR_CLIENT_ID = '28ab979e3cf4d82d';
 const AUTHGEAR_ENDPOINT = 'https://gigscourt.authgear.cloud';
 
 console.log('📱 Environment:', isCapacitor ? 'Capacitor' : 'Web');
